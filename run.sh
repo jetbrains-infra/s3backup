036d4a5f0fd05a68e8c9b53ce5b98a822acb5d39
Andrey Sizov <andrey.sizov@jetbrains.com>
2017-10-03 15:28:35 +0300

    Changed copy to sync to delete obsolete files.

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ for BUCKET in `aws s3api list-buckets | jq '.Buckets[].Name' | tr -d \"`; do
     tags=$(aws s3api get-bucket-tagging --bucket ${BUCKET} 2&>/dev/null | jq -c '.[][] | {(.Key): .Value}' | tr '\n' '\t' | grep Backup)
     if [[ $? -eq 0 ]]; then
         echo $tags
-        /usr/bin/rclone copy ORIGIN:${BUCKET} BACKUP:${BUCKET}-backup -v --ignore-checksum
-        /usr/bin/rclone copy BACKUP:${BUCKET}-backup BACKUP:${BUCKET}-backup-snapshots/${DATE}/ -v --ignore-checksum
+        /usr/bin/rclone sync ORIGIN:${BUCKET} BACKUP:${BUCKET}-backup -v --ignore-checksum
+        /usr/bin/rclone sync BACKUP:${BUCKET}-backup BACKUP:${BUCKET}-backup-snapshots/${DATE}/ -v --ignore-checksum
         AWS_ACCESS_KEY_ID=${RCLONE_CONFIG_BACKUP_ACCESS_KEY_ID}
         AWS_SECRET_ACCESS_KEY=${RCLONE_CONFIG_BACKUP_SECRET_ACCESS_KEY}
         aws s3api put-bucket-lifecycle-configuration --bucket ${BUCKET}-backup-snapshots --lifecycle-configuration file:///s3-lifecycle.json
