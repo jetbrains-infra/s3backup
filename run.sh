c8eb7d7a5df671373c85f6bf46b6fec94364b84f
Andrey Sizov <andrey.sizov@jetbrains.com>
2017-10-04 12:42:10 +0300

    Info messages have been added.

--- run.sh
+++ run.sh
@@ -8,11 +8,14 @@ for BUCKET in `aws s3api list-buckets | jq '.Buckets[].Name' | tr -d \"`; do
     AWS_SECRET_ACCESS_KEY=${RCLONE_CONFIG_ORIGIN_SECRET_ACCESS_KEY}
     tags=$(aws s3api get-bucket-tagging --bucket ${BUCKET} 2&>/dev/null | jq -c '.[][] | {(.Key): .Value}' | tr '\n' '\t' | grep Backup)
     if [[ $? -eq 0 ]]; then
-        echo $tags
+        echo "Got backup tags: $tags, working..."
+        echo "Starting sync ORIGIN:${BUCKET} BACKUP:${BUCKET}-backup"
         /usr/bin/rclone sync ORIGIN:${BUCKET} BACKUP:${BUCKET}-backup -v --ignore-checksum
+        echo "Creating snapshot to BACKUP:${BUCKET}-backup-snapshots/${DATE}/"
         /usr/bin/rclone sync BACKUP:${BUCKET}-backup BACKUP:${BUCKET}-backup-snapshots/${DATE}/ -v --ignore-checksum
         AWS_ACCESS_KEY_ID=${RCLONE_CONFIG_BACKUP_ACCESS_KEY_ID}
         AWS_SECRET_ACCESS_KEY=${RCLONE_CONFIG_BACKUP_SECRET_ACCESS_KEY}
+        echo "Updating ${BUCKET}-backup-snapshots bucket's lifecycle configuration"
         aws s3api put-bucket-lifecycle-configuration --bucket ${BUCKET}-backup-snapshots --lifecycle-configuration file:///s3-lifecycle.json
     fi
 done
